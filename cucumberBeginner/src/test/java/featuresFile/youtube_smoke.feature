6603bcddb63ac0e867ca6c7cdde116e75b1b4088
Nivas Sahoo <sahoonivas@gmail.com>
2019-03-26 09:32:51 +0530

    object class

--- cucumberBeginner/src/test/java/featuresFile/youtube_smoke.feature
+++ cucumberBeginner/src/test/java/featuresFile/youtube_smoke.feature
@@ -4,12 +4,12 @@ Description: The purpose of this feature is to test smoke for mobile you tube
 Background: User has launched youtube
 	Given User has landed in youtube homepage
 
-@YouTube
+
   Scenario: User should be able to check a dry run on youtube
     Given User is on utube landing page
     When User taps on trending image
     Then Trending related videos are shown
-    When User taps on trending image
+    When User taps on Account image
     Then Account Page is displayed
     When user taps on three dots for options
     Then Below values are displayed:
---
+++ cucumberBeginner/src/test/java/pageClass/youtubePage.java
@@ -0,0 +1,69 @@
+package pageClass;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import stepDefs.base;
+
+
+public class youtubePage extends base
+{
+	
+	WebDriver driver;
+	static WebDriverWait wait;
+	static boolean b;
+	 
+	 
+	 public youtubePage(WebDriver driver)
+	 {
+		// TODO Auto-generated constructor
+		 this.driver=driver;
+		 PageFactory.initElements(driver, this);
+		 wait=new WebDriverWait(driver, 40);
+
+	 }
+	 
+	
+	 
+	 @FindBy(xpath="//h1[text()='Home']")
+	 WebElement icon_utubeHome;
+	 
+	 
+	@FindBy(xpath="//a[contains(@href,'trending')]")
+	WebElement lbl_trendingIcon;
+	
+	@FindBy(xpath="//a[@aria-label='Trending']")  //aria-selected="true"
+	 WebElement icon_trendingIcon;
+	
+	@FindBy(xpath="//a[@aria-label='Account']")
+	 WebElement lbl_accountIcon;
+	
+	
+	@FindBy(xpath="//h1[text()='Account']")
+	WebElement icon_accountIcon;
+	
+	@FindBy(xpath="//button[@aria-label='Settings']")
+	 WebElement optionImg;
+	
+	@FindBy(xpath="//android.widget.Button[@text='Sign In']")
+	 WebElement signInOption;
+	
+	@FindBy(xpath="//android.widget.Button[@text='Help']")
+	 WebElement helpOption;
+	
+	@FindBy(xpath="//android.widget.Button[@text='Desktop']")
+	 WebElement desktopOption;
+	
+	@FindBy(xpath="//android.widget.Button[@text='Cancel']")
+	 WebElement canclOption;
+	
+	
+
+	
+	
+	 
+	 
+}
